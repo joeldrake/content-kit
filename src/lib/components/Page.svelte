2dc5b1fe07c6b379126c536c3fe821ebaf1c72fa
Joel Drake <joel.drake@ica.se>
2022-04-27 15:19:45 +0200

    Doing nested url params in same file

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
-	export let page: App.Entry;
+	export let page: App.Entry | undefined;
 </script>
 
-<h1>{page.fields.title}</h1>
+{#if page}
+	<h1>{page.fields.title}</h1>
 
-{#if page.fields.image}
-	<img
-		src={page.fields.image.fields.file.url}
-		width={page.fields.image.fields.file.details.image.width}
-		height={page.fields.image.fields.file.details.image.height}
-		alt={page.fields.image.fields.description}
-	/>
-{/if}
-{#if page.fields.content}
-	<div>{@html documentToHtmlString(page.fields.content).replace(/\n/g, `<br />`)}</div>
+	{#if page.fields.image}
+		<img
+			src={page.fields.image.fields.file.url}
+			width={page.fields.image.fields.file.details.image.width}
+			height={page.fields.image.fields.file.details.image.height}
+			alt={page.fields.image.fields.description}
+		/>
+	{/if}
+	{#if page.fields.content}
+		<div>{@html documentToHtmlString(page.fields.content).replace(/\n/g, `<br />`)}</div>
+	{/if}
 {/if}
---
+++ src/routes/[...page]/index.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ params }) {
+		const [page, subpage] = params.page.split('/');
+		return {
+			props: {
+				page,
+				subpage
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Page from '$lib/components/Page.svelte';
+	import { pages } from '$lib/store';
+	export const prerender = true;
+	export let page: string | undefined;
+	export let subpage: string | undefined;
+	let findSubpage: App.Entry | undefined;
+	let findPage: App.Entry | undefined;
+	$: findPage = $pages?.fields.subpages?.find((p) => p.fields.slug === page);
+	$: if (findPage && subpage) {
+		findSubpage = findPage.fields.subpages?.find((p) => p.fields.slug === subpage);
+	}
+</script>
+
+page: {page}
+subpage: {subpage}
+
+{#if findSubpage || findPage}
+	<Page page={findSubpage || findPage} />
+{/if}
