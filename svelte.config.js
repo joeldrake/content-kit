8aab5bfbf0ab632828f9cbeb7c30d81b71ed94fe
Joel Drake <joel.drake@ica.se>
2022-05-25 10:54:25 +0200

    prebuilt

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +10,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			default: true
+		},
 		vite: {
 			define: {
 				'process.env': process.env
